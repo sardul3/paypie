b7c3501c6d48b1ddbcd95cec10a3d8e9f6aac21d
sagarpoudel <poudelsagar530@gmail.com>
2025-04-06 13:57:03 -0500

    Expense group should reject empty names [RED]

--- src/test/java/io/github/sardul3/ExpenseGroupTest.java
+++ src/test/java/io/github/sardul3/ExpenseGroupTest.java
@@ -1,15 +1,26 @@
 package io.github.sardul3;
 
+import io.github.sardul3.account.domain.ExpenseGroup;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExpenseGroupTest {
 
     @Test
-    void expenseGroupShouldBeCreated() {
+    @DisplayName("Expense Group | should be created without errors")
+    void expenseGroupShouldBeCreatedTest() {
         final String GROUP_NAME = "test-expense-group";
         ExpenseGroup expenseGroup = ExpenseGroup.withName(GROUP_NAME);
         assertEquals(GROUP_NAME, expenseGroup.getName());
     }
+
+    @Test
+    @DisplayName("Expense Group | should not be created for empty name")
+    void expenseGroupShouldNotBeEmptyTest() {
+        assertThrows(IllegalArgumentException.class, () -> ExpenseGroup.withName(""));
+    }
+
 }
