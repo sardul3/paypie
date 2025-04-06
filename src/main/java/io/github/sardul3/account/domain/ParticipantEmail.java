f530c823735e77b1704143143286662ce54df230
sagarpoudel <poudelsagar530@gmail.com>
2025-04-06 17:33:37 -0500

    Participant email should be equal if they have same value  [GREEN]

--- src/main/java/io/github/sardul3/account/domain/ParticipantEmail.java
+++ src/main/java/io/github/sardul3/account/domain/ParticipantEmail.java
@@ -24,6 +24,19 @@ public class ParticipantEmail {
         return email;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParticipantEmail that = (ParticipantEmail) o;
+        return Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+
     private static boolean isValid(String email) {
         if (email.isEmpty()) return false;
 
