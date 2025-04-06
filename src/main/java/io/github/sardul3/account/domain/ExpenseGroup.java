6a6830859ce2af9d7de717c89dcff2ecfd1d1102
sagarpoudel <poudelsagar530@gmail.com>
2025-04-06 14:15:05 -0500

    Group Expense name should be limited to 50 chars [GREEN]

--- src/main/java/io/github/sardul3/account/domain/ExpenseGroup.java
+++ src/main/java/io/github/sardul3/account/domain/ExpenseGroup.java
@@ -1,11 +1,19 @@
 package io.github.sardul3.account.domain;
 
 public class ExpenseGroup {
+    private static final int MAX_GROUP_NAME_LENGTH_LIMIT = 50;
+
     private final String name;
+
 
     public ExpenseGroup(String name) {
         if(name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if(name.length() > 50) {
+            throw new IllegalArgumentException(
+                    String.format("Name cannot be longer than {} characters",
+                            MAX_GROUP_NAME_LENGTH_LIMIT));
         }
         this.name = name;
     }
