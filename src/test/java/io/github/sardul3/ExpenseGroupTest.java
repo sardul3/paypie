171f845a19a7cb0eb74c78aeb1ea22196a75456f
sagarpoudel <poudelsagar530@gmail.com>
2025-04-06 21:09:59 -0500

    Enhance the group creation process and only active it once there are atleast 2 members [RED]

--- src/test/java/io/github/sardul3/ExpenseGroupTest.java
+++ src/test/java/io/github/sardul3/ExpenseGroupTest.java
@@ -2,6 +2,7 @@ package io.github.sardul3;
 
 import io.github.sardul3.account.domain.ExpenseGroup;
 import io.github.sardul3.account.domain.GroupName;
+import io.github.sardul3.account.domain.Participant;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -15,5 +16,18 @@ public class ExpenseGroupTest {
 
         assertThat(expenseGroup.getGroupName())
                 .isEqualTo(groupName);
+    }
+
+    void expenseGroupShouldHaveAtleastTwoMembers() {
+        GroupName groupName = GroupName.withName("demo");
+        Participant createdBy = Participant.withEmail("creator@example.com");
+        Participant participant = Participant.withEmail("user@example.com");
+        ExpenseGroup expenseGroup = ExpenseGroup.from(
+                groupName, createdBy
+        );
+
+        assertThat(expenseGroup.isActivated())
+                .isFalse();
+
     }
 }
