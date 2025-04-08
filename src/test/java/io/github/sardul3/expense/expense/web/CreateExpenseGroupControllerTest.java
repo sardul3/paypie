17e914ba168fb342bd51fd0cbb8bf4c13c136947
sagarpoudel <poudelsagar530@gmail.com>
2025-04-07 20:34:29 -0500

    Make the REST controller endpoint call the actual usecase from the input ports from app layer [RED]

--- src/test/java/io/github/sardul3/expense/expense/web/CreateExpenseGroupControllerTest.java
+++ src/test/java/io/github/sardul3/expense/expense/web/CreateExpenseGroupControllerTest.java
@@ -2,17 +2,29 @@ package io.github.sardul3.expense.expense.web;
 
 
 import io.github.sardul3.expense.adapter.in.web.CreateExpenseGroupController;
+import io.github.sardul3.expense.application.port.in.CreateExpenseGroupUseCase;
 import io.github.sardul3.expense.domain.model.ExpenseGroup;
 import io.github.sardul3.expense.domain.model.Participant;
 import io.github.sardul3.expense.domain.valueobject.GroupName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class CreateExpenseGroupControllerTest {
+
+    @Mock
+    private CreateExpenseGroupUseCase createExpenseGroupUseCase;
+
+    @InjectMocks
+    private CreateExpenseGroupController createExpenseGroupController;
 
     @Test
     void shouldReturn201CreatedHttpResponseEntityForHappyPath() {
@@ -26,5 +38,6 @@ public class CreateExpenseGroupControllerTest {
 
         assertNotNull(response);
         assertThat(response.getStatusCodeValue()).isEqualTo(HttpStatus.CREATED.value());
+        verify(createExpenseGroupUseCase.createExpenseGroup(any()), times(1));
     }
 }
