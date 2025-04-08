fc98d219fbcc446afe5ff33486f018d0cfac096a
sagarpoudel <poudelsagar530@gmail.com>
2025-04-08 09:32:23 -0500

    Remove the unused dependencies from the test file

--- src/test/java/io/github/sardul3/expense/expense/web/CreateExpenseGroupControllerTest.java
+++ src/test/java/io/github/sardul3/expense/expense/web/CreateExpenseGroupControllerTest.java
@@ -1,33 +1,21 @@
 package io.github.sardul3.expense.expense.web;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.sardul3.expense.adapter.in.web.controller.CreateExpenseGroupController;
 import io.github.sardul3.expense.adapter.in.web.dto.CreateExpenseGroupRequest;
-import io.github.sardul3.expense.application.dto.CreateExpenseGroupCommand;
 import io.github.sardul3.expense.application.dto.CreateExpenseGroupResponse;
 import io.github.sardul3.expense.application.port.in.CreateExpenseGroupUseCase;
-import io.github.sardul3.expense.domain.model.ExpenseGroup;
-import io.github.sardul3.expense.domain.model.Participant;
-import io.github.sardul3.expense.domain.valueobject.GroupName;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -45,9 +33,6 @@ public class CreateExpenseGroupControllerTest {
 
     @MockBean
     private CreateExpenseGroupUseCase createExpenseGroupUseCase;
-
-//    @InjectMocks
-//    private CreateExpenseGroupController createExpenseGroupController;
 
     @Test
     void shouldReturn201CreatedHttpResponseEntityForHappyPath() throws Exception {
