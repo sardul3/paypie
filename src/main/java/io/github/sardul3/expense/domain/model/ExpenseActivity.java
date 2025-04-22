02f3b1a638f03489604b6115c76ef7b4e797be58
sagarpoudel <poudelsagar530@gmail.com>
2025-04-22 12:08:52 -0500

    Expense activity should contain valid descriptions [REFACTOR]

--- src/main/java/io/github/sardul3/expense/domain/model/ExpenseActivity.java
+++ src/main/java/io/github/sardul3/expense/domain/model/ExpenseActivity.java
@@ -3,6 +3,8 @@ package io.github.sardul3.expense.domain.model;
 import io.github.sardul3.expense.domain.valueobject.Money;
 
 public class ExpenseActivity {
+    private static final int MAX_DESCRIPTION_LENGTH = 50;
+
     private final String description;
     private final Money amount;
 
@@ -12,21 +14,28 @@ public class ExpenseActivity {
     }
 
     public static ExpenseActivity from(String description, Money amount) {
+        validateExpenseAmount(amount);
+        validateExpenseDescription(description);
+        return new ExpenseActivity(description, amount);
+    }
+
+    private static void validateExpenseAmount(Money amount) {
         if(amount.isNotPositive()) {
            throw new IllegalArgumentException("Expense amount must be positive and non-zero");
         }
+    }
+
+    private static void validateExpenseDescription(String description) {
         if(description == null) {
             throw new IllegalArgumentException("Expense description cannot be null");
         }
         if(description.trim().isEmpty()) {
             throw new IllegalArgumentException("Expense description cannot be empty");
         }
-        if(description.length() > 50) {
+        if(description.length() > MAX_DESCRIPTION_LENGTH) {
             throw new IllegalArgumentException("Expense description cannot be longer than 50 characters");
         }
-        return new ExpenseActivity(description, amount);
     }
-
 
 
 }
